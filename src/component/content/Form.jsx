46016da62498415f25ac2a92532b70bd7c9ccfd3
Qian Linfeng <thewawar@gmail.com>
2016-04-18 16:38:01 +0800

    Remove old FormModal class

--- src/component/content/Form.jsx
+++ src/component/content/Form.jsx
@@ -101,15 +101,20 @@ export class BaseForm extends Component {
     });
   }
 
+  getFormItems() {
+    const { fields, items } = this.props;
+    return fields.map((field) => {
+      return items[field].render.call(this);
+    });
+  }
+
   render() {
-    const {form, formProps, fields, items, labelCol, wrapperCol} = this.props;
+    const {form, formProps, labelCol, wrapperCol} = this.props;
     const footerItem = <FormItem wrapperCol={{ span: wrapperCol, offset: labelCol }}>
       <Button className="list-btn" type="primary" onClick={this.handleSubmit.bind(this)}>提交</Button>
       <Button className="list-btn" type="ghost" onClick={this.handleReset.bind(this)}>重置</Button>
     </FormItem>;
-    let formItems = fields.map((field) => {
-      return items[field].render.call(this);
-    });
+    let formItems = this.getFormItems();
     formItems.push(footerItem);
     return (
       <Form form={form} onSubmit={e => this.handleSubmit(e)}
@@ -132,10 +137,8 @@ export class FormModal extends BaseForm {
   })
 
   render() {
-    const {form, formProps, fields, items} = this.props;
-    const formItems = fields.map((field) => {
-      return items[field].render.call(this);
-    });
+    const {form, formProps} = this.props;
+    const formItems = this.getFormItems();
     const footer = <div>
       <Button type="ghost" onClick={e => this.handleReset()}>重置</Button>
       <Button type="primary" onClick={e => this.handleSubmit(e)}>提交</Button>
@@ -144,119 +147,6 @@ export class FormModal extends BaseForm {
       <Modal title={this.props.title}
              visible={this.props.visible}
              footer={footer}
-             onCancel={e => this.props.onCancel()}>
-        <Form form={form} onSubmit={e => this.handleSubmit(e)}
-              {...formProps} >
-          {formItems}
-        </Form>
-      </Modal>
-    );
-  }
-}
-
-export class _FormModal extends Component {
-  static propTypes = {
-    /* Modal */
-    visible: PropTypes.bool.isRequired,
-    title: PropTypes.string,
-    onCancel: PropTypes.func, /* function onCancel() */
-    /* Form */
-    type: PropTypes.string.isRequired,
-    labelCol: PropTypes.number,
-    wrapperCol: PropTypes.number,
-    formProps: PropTypes.object,
-    fields: PropTypes.array.isRequired,
-    items: PropTypes.object.isRequired,
-    object: PropTypes.object,
-    onSubmit: PropTypes.func, /* function onSubmit(values, form) */
-    onSuccess: PropTypes.func, /* function onSuccess() */
-  }
-  static defaultProps = {
-    /* Modal */
-    title: "表单",
-    /* Form */
-    labelCol: 6,
-    wrapperCol: 14,
-    formProps: {horizontal: true},
-    object: {},
-    onSuccess: function() {}
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {object: this.props.object};
-  }
-
-  componentWillReceiveProps(newProps) {
-    console.log('componentWillReceiveProps', this.props.type, newProps);
-    if (!_.isEqual(newProps.object, this.props.object)) {
-      this.setFieldDefaults(newProps.object);
-      this.setState({object: newProps.object});
-    }
-  }
-
-  formatObject(object) {
-    let result = {};
-    const { fields } = this.props;
-    fields.map(function(field) {
-      let value = object[field];
-      if (_.isBoolean(value)) {
-        value = value ? '1' : '0';
-      } else if (_.isNumber(value)) {
-        value = String(value);
-      }
-      result[field] = value;
-    });
-    console.log('fields:', fields, 'result', result);
-    return result;
-  }
-
-  setFieldDefaults(object) {
-    console.log('setFieldDefaults', this.props.type, object);
-    const newObject = object === undefined ? this.state.object : object;
-    this.props.form.setFieldsValue(this.formatObject(newObject));
-  }
-
-  handleReset() {
-    this.props.form.resetFields();
-    this.setFieldDefaults();
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const { form, onSubmit, onSuccess, object } = this.props;
-    const theOnSubmit = _.get(this, 'onSubmit', onSubmit);
-    form.validateFieldsAndScroll((errors, values) => {
-      if (!!errors) {
-        console.log('Errors in form!!!', errors);
-        return;
-      }
-      values.id = object.id;
-      theOnSubmit.bind(this)(values, form, (newObject) => {
-        this.setState({object: newObject}, () => {
-          this.handleReset();
-          onSuccess();
-        });
-      });
-      console.log('Submit!!!', values);
-    });
-  }
-
-  render() {
-    const {form, formProps, fields, items} = this.props;
-    const formItems = fields.map((field) => {
-      return items[field].render.call(this);
-    });
-    const ModalFooter = this.props.type === "create" ? (<div>
-        <Button type="ghost" onClick={e => this.handleReset()}>重置</Button>
-        <Button type="primary" onClick={e => this.handleSubmit(e)}>提交</Button>
-      </div>) : (<div>
-        <Button type="primary" onClick={e => this.handleSubmit(e)}>提交</Button>
-      </div>);
-    return (
-      <Modal title={this.props.title}
-             visible={this.props.visible}
-             footer={ModalFooter}
              onCancel={e => this.props.onCancel()}>
         <Form form={form} onSubmit={e => this.handleSubmit(e)}
               {...formProps} >
